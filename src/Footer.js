07d248caab5becba573985964c62850843046199
Mark Boas <mark.boas@gmail.com>
2018-02-07 19:57:09 +0100

    added footer.js

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => (
+  <Footer style={{ textAlign: 'center' }}>
+    <a href="https://github.com/BadIdeaFactory/contextubot">The Glorious Contextubot</a> — created by <a href="https://github.com/BadIdeaFactory">Bad Idea Factory</a>
+  </Footer>
+)
+
+export default Footer
