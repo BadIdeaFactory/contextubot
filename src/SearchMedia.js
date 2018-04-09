3771a8f969a0ad9e631c8fd5ce8ed811c7df6c7b
Laurian Gridinoc <laurian@gridinoc.name>
2018-04-09 15:44:13 +0800

    LOL

--- src/SearchMedia.js
+++ src/SearchMedia.js
@@ -283,7 +283,10 @@ class SearchMedia extends Component {
       const renderResult = match => {
         if (!match.duration || match.duration === 0) return null;
 
-        const uid = match.source.replace('.afpt', '').replace('_tva', '').replace('/data/archive/week/selected/', '');
+        const uid = match.source
+          .replace('.afpt', '')
+          .replace('_tva', '')
+          .replace('/data/archive/week/selected/', '');
         const clipStart = match.time;
         const clipEnd = match.time + match.duration;
         const mp4Url = `https://archive.org/download/${uid}/${uid}.mp4?t=${clipStart}/${clipEnd}`;
