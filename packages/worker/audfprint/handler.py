17e1e309d9516aed4d6929cfa6dcf112194ac799
Laurian Gridinoc <laurian@gridinoc.name>
2018-10-02 20:47:57 +0200

    TVA hash/db creation

--- packages/worker/audfprint/handler.py
+++ packages/worker/audfprint/handler.py
@@ -1,3 +1,7 @@
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore')
+
 try:
     import unzip_requirements
 except ImportError:
@@ -6,11 +10,11 @@ except ImportError:
 import os
 import sys
 import json
+from datetime import date, timedelta
 
 import boto3
 import botocore
 from aws_xray_sdk.core import xray_recorder
-# from aws_xray_sdk.core import patch
 from aws_xray_sdk.core import patch_all
 
 import numpy as np
@@ -19,19 +23,30 @@ import audfprint_match
 import audfprint_analyze
 import hash_table
 
-# patch(['boto3'])
 patch_all()
 
 s3 = boto3.resource('s3')
+s3client = boto3.client('s3')
 BUCKET_NAME = os.environ['BUCKET_NAME']
 
 
-def create(event, context):
-    key = event.Records[0].s3.object.key
+def fingerprint(event, context):
+    print(event)
+
+    body = {
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+    return response
+
 
 def create(event, context):
-    # key = event.Records[0].s3.object.key
-    s3.Bucket(BUCKET_NAME).download_file('_test-fprint.afpt', '/tmp/_test-fprint.afpt')
+    day = (date.today() - timedelta(2)).strftime('%Y%m%d')
+    channel = event['channel']
 
     analyzer = audfprint_analyze.Analyzer()
     analyzer.n_fft = 512
@@ -43,21 +58,24 @@ def create(event, context):
     analyzer.verbose = False
 
     # hashbits=20, depth=100, maxtime=16384
-    hash_tab = hash_table.HashTable(
-                    hashbits=20,
-                    depth=100,
-                    maxtime=16384)
+    hash_tab = hash_table.HashTable(hashbits=20, depth=100, maxtime=262144)
     hash_tab.params['samplerate'] = analyzer.target_sr
 
-    analyzer.ingest(hash_tab, '/tmp/_test-fprint.afpt')
+    fingerprints = s3client.list_objects_v2(Bucket=BUCKET_NAME, Prefix='tva/{}/{}/'.format(day, channel))['Contents']
+
+    for fingerprint in fingerprints:
+        s3.Bucket(BUCKET_NAME).download_file(fingerprint['Key'], '/tmp/{}'.format(fingerprint['Key'].split('/').pop()))
+        analyzer.ingest(hash_tab, '/tmp/{}'.format(fingerprint['Key'].split('/').pop()))
+        os.remove('/tmp/{}'.format(fingerprint['Key'].split('/').pop()))
 
     if hash_tab and hash_tab.dirty:
-        hash_tab.save('/tmp/_test-db.pklz')
+        hash_tab.save('/tmp/{}-{}.pklz'.format(channel, day))
 
-    s3.Bucket(BUCKET_NAME).upload_file('/tmp/_test-db.pklz', '_test-db.pklz')
+    s3.Bucket(BUCKET_NAME).upload_file('/tmp/{}-{}.pklz'.format(channel, day), 'hash/{}/{}-{}.pklz'.format(day, channel, day))
 
     body = {
-        "input": event
+        "input": event,
+        "fingerprints": len(fingerprints)
     }
 
     response = {
@@ -65,6 +83,7 @@ def create(event, context):
         "body": json.dumps(body)
     }
     return response
+
 
 def match(event, context):
     s3.Bucket(BUCKET_NAME).download_file('_test-fprint.afpt', '/tmp/_test-fprint.afpt')
