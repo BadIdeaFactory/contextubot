df416f51050391150ec694e3482982bf4e81d74f
Laurian Gridinoc <laurian@gridinoc.name>
2018-10-04 18:07:36 +0200

    WIP call fargate

--- packages/api/extract.js
+++ packages/api/extract.js
@@ -10,6 +10,7 @@ AWSXRay.capturePromise();
 const AWS = AWSXRay.captureAWS(AwsSdk);
 
 const s3 = new AWS.S3();
+const ECS = new AWS.ECS();
 
 
 export const audio = async (event, context, cb) => {
@@ -50,6 +51,52 @@ export const audio = async (event, context, cb) => {
     Bucket: process.env.BUCKET_NAME,
     Key: `wave/${id}/audio.wav`,
   }).promise();
+
+  const task = await promisify(ECS.runTask).runECSTask({
+    cluster: `${process.env.ECS_CLUSTER_NAME}`,
+    launchType: 'FARGATE',
+    taskDefinition: `${process.env.ECS_TASK_DEFINITION}`,
+    count: 1,
+    platformVersion:'LATEST',
+    networkConfiguration: {
+      awsvpcConfiguration: {
+          subnets: [
+              `${process.env.ECS_TASK_VPC_SUBNET_1}`,
+              `${process.env.ECS_TASK_VPC_SUBNET_2}`
+          ],
+          assignPublicIp: 'ENABLED'
+      }
+    },
+    overrides: {
+      containerOverrides: [
+        {
+          name: 'ffmpeg-thumb',
+          environment: [
+            // {
+            //   name: 'INPUT_VIDEO_FILE_URL',
+            //   value: `${s3_video_url}`
+            // },
+            // {
+            //   name: 'OUTPUT_THUMBS_FILE_NAME',
+            //   value: `${thumbnail_file}`
+            // },
+            // {
+            //   name: 'POSITION_TIME_DURATION',
+            //   value: `${frame_pos}`
+            // },
+            // {
+            //   name: 'OUTPUT_S3_PATH',
+            //   value: `${OUTPUT_S3_PATH}`
+            // },
+            // {
+            //   name: 'AWS_REGION',
+            //   value: `${process.env.AWS_REGION}`
+            // }
+          ]
+        }
+      ]
+    }
+  });
 
   cb(null, {
     statusCode: 200,
